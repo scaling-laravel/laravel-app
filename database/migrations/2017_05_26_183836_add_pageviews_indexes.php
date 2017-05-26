bc25e19c440146c27b1870040ddfd954c20a57cf
Chris Fidao <fideloper@gmail.com>
2017-05-26 13:41:02 -0500

    add the indexes we end up with after the testing process

---
+++ database/migrations/2017_05_26_183836_add_pageviews_indexes.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPageviewsIndexes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pageviews', function (Blueprint $table) {
+            # $table->index(['user_id', 'domain']); // We dropped this one, [domain, user_id] was less efficient
+            $table->index(['user_id', 'customer_id']);
+            $table->index(['domain', 'user_id']);
+            $table->index(['user_id', 'customer_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pageviews', function (Blueprint $table) {
+            //
+        });
+    }
+}
